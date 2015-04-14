6c94088d77e6480edcd6941466115ba565ddeb80
zatricion <zatricion@gmail.com>
2015-04-14 00:31:42 -0700

    more testing

--- kademlia/node/cove.py
+++ kademlia/node/cove.py
@@ -1,15 +1,16 @@
 from __future__ import absolute_import, unicode_literals
 
-from celery.bin import worker
+from celery.bin import worker as w
 from celery import current_app
+from celery import task
 
 @task
 def test():
     print "hello"
 
-def start(self):
-    self.app = current_app._get_current_object()
-    self.app.config_from_object('celeryconfig') 
+def start():
+    app = current_app._get_current_object()
+    app.config_from_object('celeryconfig') 
 
-    self.worker = worker.worker(app=self.app)
-    self.worker.run()
+    worker = w.worker(app=app)
+    worker.run()
