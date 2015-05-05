b32fbdaaa174bed63e2aa85c9cebaf2b499b9ae3
zatricion <zatricion@gmail.com>
2015-05-04 21:18:59 -0700

    federation works once only

--- deploy/cove.py
+++ deploy/cove.py
@@ -19,13 +19,17 @@ class Cove(object):
                                    routing_key=q_name,
                                    body=str(message))
                                    # body=msgpack.packb(message))
+        print "send", message, "to", q_name
 
     def receive(self, q_name, callback):
+        print "receive from", q_name
         self.channel.queue_declare(queue=q_name)
 
         for method_frame, properties, body in self.channel.consume(q_name):
             # Acknowledge the message
             self.channel.basic_ack(method_frame.delivery_tag)
-
-            callback(body)
+            
+            print "received", eval(body), "from", q_name
+            
+            callback(eval(body))
             # callback(msgpack.unpackb(body, use_list=False))
