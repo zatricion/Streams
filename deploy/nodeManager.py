25b24bb11ad9da6b742e33f27644900aa1d70eba
zatricion <zatricion@gmail.com>
2015-06-21 23:29:19 -0700

    testing nodeManager

--- deploy/nodeManager.py
+++ deploy/nodeManager.py
@@ -1,6 +1,9 @@
 from multiprocessing import Process
 import subprocess as sp
 
+def printer(res):
+    print res
+    
 class NodeManager(Process):
     """
     Manages a node (don't know how yet).
@@ -12,9 +15,6 @@ class NodeManager(Process):
             self.hostname = f.read()
     
     def run(self):
-        self.server.get('deploy_config').addCallback(self.printer)
+        self.server.get('deploy_config').addCallback(printer)
         sp.call(["python", "runAnomaly.py", "deploy_test.any", "deploy_test.config", self.hostname])
         # sp.call(["python", "start_network.py"])
-        
-    def printer(self, res):
-        print res
