78b5fde2483db061db2e92a1eeb26a63a32015a6
zatricion <zatricion@gmail.com>
2016-03-19 21:55:29 -0700

    blank

--- deploy/node.py
+++ deploy/node.py
@@ -1,6 +1,7 @@
 from twisted.application import service, internet
 from twisted.python.log import ILogObserver
 from twisted.internet import reactor, task
+import twisted
 
 import sys, os
 sys.path.append(os.path.dirname(__file__))
