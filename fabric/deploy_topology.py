7656353f8a736d57afbf137e918399ac176b8f9c
zatricion <zatricion@gmail.com>
2016-03-19 23:31:39 -0700

    rabbitmq still squirrelly, but kademlia is working

--- fabric/deploy_topology.py
+++ fabric/deploy_topology.py
@@ -15,14 +15,16 @@ env.key_filename = '~/.ssh/id_rsa'
 env.abort_on_prompts = True
 env.hostnames = {}
 env.addrs = {}
+env.ipaddrs = {}
 
 def populate_hosts():
     for line in open(HOSTS, 'r'):
         if line.strip() and not line.startswith('#'):
             name, host, password = line.split()
             env.hosts.append(host)
-            # env.passwords[host+':22'] = password
-            env.hostnames[host.split('@')[1]] = name
+            ip = host.split('@')[1]
+            env.hostnames[ip] = name
+            env.ipaddrs[name] = ip
             env.addrs[name] = host
 
 @task
@@ -41,9 +43,6 @@ def deploy_streams():
         
     with cd('Streams/deploy'):
         run('echo {0} > hostname.txt'.format(env.hostnames[env.host]))
-    
-        ## TODO: allow specification of a real config file
-        run('python runAnomaly.py deploy_test.any deploy_test.config {0}'.format(env.hostnames[env.host]))
 
 @task
 def start_kademlia(ip, port=None):
@@ -51,13 +50,13 @@ def start_kademlia(ip, port=None):
         with settings(warn_only=True):
             run('kill `cat twistd.pid`')
         run('twistd -l deploy_test.log node -b {0} -p {1}'.format(ip, port))
-        
+
 @task
 def start_kademlia_verbose(ip, port=None):
     with cd('Streams/deploy/'):
         with settings(warn_only=True):
             run('kill `cat twistd.pid`')
-        run('twistd -l deploy_test.log -n node -b {0} -p {1}'.format(ip, port))
+        run('twistd -n node -b {0} -p {1}'.format(ip, port))
         
 def create_local_node(port=None):
     with lcd('../deploy/'):
@@ -71,23 +70,17 @@ def kill_local_node():
  
 def main():
   # start temporary kademlia node
-  create_local_node(KADEMLIA_PORT)
-  
-  time.sleep(15)
-  print "go"
-
+  # create_local_node(KADEMLIA_PORT)
   # get local ipv4 address for bootstrapping (deploying from macbook)
-  my_ipv4 = local('ipconfig getifaddr en0', capture=True)
+  # my_ipv4 = local('ipconfig getifaddr en0', capture=True)
   
+
   # Get the repo
   execute(deploy_streams)
 
   # start Kademlia network
-  execute(start_kademlia, my_ipv4, KADEMLIA_PORT, hosts=env.addrs["rpi0"]) 
-  execute(start_kademlia, my_ipv4, KADEMLIA_PORT, hosts=env.addrs["rpi1"]) 
-
-  # clean up
-  kill_local_node()
+  execute(start_kademlia, env.ipaddrs["rpi1"], KADEMLIA_PORT, hosts=env.addrs["rpi0"]) 
+  execute(start_kademlia, env.ipaddrs["rpi0"], KADEMLIA_PORT, hosts=env.addrs["rpi1"]) 
 
 if __name__ == '__main__':
   populate_hosts()
