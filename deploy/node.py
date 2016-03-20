91eeec1114981f295d5447c9568b542ceebd4c2d
zatricion <zatricion@gmail.com>
2016-03-19 21:27:25 -0700

    logging

--- deploy/node.py
+++ deploy/node.py
@@ -1,13 +1,11 @@
 from twisted.application import service, internet
 from twisted.python.log import ILogObserver
 from twisted.internet import reactor, task
-import logging
-logging.basicConfig(filename='node.log',level=logging.DEBUG)
+from twisted.python import log
 
 import sys, os
 sys.path.append(os.path.dirname(__file__))
 from kademlia.network import Server
-from kademlia import log
 
 import subprocess as sp
 from nodeManager import NodeManager
@@ -19,10 +17,10 @@ def printer(res):
     
 def bootstrapDone(found, server, port):
     raise Exception("bootstrapDone called")
-    logging.info("attempting to set deploy config")
+    log.info("attempting to set deploy config")
     with open('../deploy/deploy_test.config', 'r') as f:
         server.set('deploy_config', f.read()).addCallback(printer) 
-    logging.info("starting NodeMananger")
+    log.info("starting NodeMananger")
     manager = NodeManager(server)
     manager.start()
 
@@ -31,7 +29,8 @@ def makeService(config):
     port = int(config["port"])
     
     kserver = Server()
-
+    
+    log.info("starting service")
     if bootstrap_addr is not None:
         bootstrap_tuple = (bootstrap_addr, port)
         kserver.bootstrap([bootstrap_tuple]).addCallback(bootstrapDone, kserver)
